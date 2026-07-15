4620427d02f3af4b1330552cdfa933c5cce56a4f
WesternSpace <80211714+WesternSpace@users.noreply.github.com>
2026-07-15 11:47:57 -0400

    Simplified plugin.

--- SE2DisableAlphaBanner/Plugin.cs
+++ SE2DisableAlphaBanner/Plugin.cs
@@ -1,45 +1,21 @@
-﻿using Keen.Game2.Game.Plugins;
-using Keen.VRage;
-using Keen.VRage.Library.Diagnostics;
+﻿using Keen.VRage.Library.Diagnostics;
 using Keen.VRage.Core.EngineComponents;
-using Keen.VRage.DCS;
-using Keen.VRage.DCS.Components;
-using Keen.VRage.Core;
-using Keen.VRage.Core.Project;
-using HarmonyLib;
-using System.Reflection;
+using Keen.VRage.Core.Plugins;
+using Keen.Game2.Client.RuntimeSystems.CoreScenes;
 
 namespace SE2DisableAlphaBanner
 {
-    public class Plugin : IPlugin, IDisposable
+    public class Plugin : IPlugin
     {
-        public const string Name = "ClientPluginTemplate";
-        public static Plugin Instance { get; private set; }
-
         public Plugin(PluginHost host)
         {
-            Instance = this;
-
-            Log.Default.WriteLine($"[Test Plugin] Loaded test plugin");
-
+            Log.Default.WriteLine($"[SE2DisableAlphaBanner] Loaded plugin");
             host.OnBeforeEngineInstantiated += Host_OnBeforeEngineInstantiated;
-            host.OnBeforeProjectsLoaded += Host_OnBeforeProjectsLoaded;
         }
 
         private void Host_OnBeforeEngineInstantiated(EngineBuilder builder)
         {
             builder.Configure<GameRenderComponentCoreConfigurationObjectBuilder>().DrawAlphaBanner = false;
-        }
-
-        private void Host_OnBeforeProjectsLoaded(List<VRageProject> list)
-        {
-            //Harmony harmony = new Harmony(Name);
-            //harmony.PatchAll(Assembly.GetExecutingAssembly());
-        }
-
-        public void Dispose()
-        {
-            Instance = null;
         }
     }
 }
