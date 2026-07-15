0c62e2f133ec0e6ef92a10c64e92a33b35877f1c
WesternSpace <80211714+WesternSpace@users.noreply.github.com>
2026-07-15 11:43:11 -0400

    Revert commit 4482658.

--- SE2DisableAlphaBanner/Plugin.cs
+++ SE2DisableAlphaBanner/Plugin.cs
@@ -1,22 +1,45 @@
 ﻿using Keen.Game2.Game.Plugins;
+using Keen.VRage;
 using Keen.VRage.Library.Diagnostics;
+using Keen.VRage.Core.EngineComponents;
+using Keen.VRage.DCS;
+using Keen.VRage.DCS.Components;
+using Keen.VRage.Core;
+using Keen.VRage.Core.Project;
 using HarmonyLib;
 using System.Reflection;
 
 namespace SE2DisableAlphaBanner
 {
-    public class Plugin : IPlugin
+    public class Plugin : IPlugin, IDisposable
     {
-        public const string Name = "SE2DisableAlphaBanner";
+        public const string Name = "ClientPluginTemplate";
+        public static Plugin Instance { get; private set; }
 
-        public Plugin()
+        public Plugin(PluginHost host)
         {
-            Log.Default.WriteLine($"[{Name}] Loaded plugin.");
+            Instance = this;
 
-            Harmony harmony = new Harmony(Name);
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Default.WriteLine($"[Test Plugin] Loaded test plugin");
 
-            Log.Default.WriteLine($"[{Name}] Applied patches");
+            host.OnBeforeEngineInstantiated += Host_OnBeforeEngineInstantiated;
+            host.OnBeforeProjectsLoaded += Host_OnBeforeProjectsLoaded;
+        }
+
+        private void Host_OnBeforeEngineInstantiated(EngineBuilder builder)
+        {
+            builder.Configure<GameRenderComponentCoreConfigurationObjectBuilder>().DrawAlphaBanner = false;
+        }
+
+        private void Host_OnBeforeProjectsLoaded(List<VRageProject> list)
+        {
+            //Harmony harmony = new Harmony(Name);
+            //harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void Dispose()
+        {
+            Instance = null;
         }
     }
 }
---
+++ SE2DisableAlphaBanner/WatermarkOverlayScreen_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Keen.Game2.Client.UI.Overlays;
+
+namespace SE2DisableAlphaBanner
+{
+    [HarmonyPatch(typeof(WatermarkOverlayScreen), MethodType.Constructor)]
+    internal class WatermarkOverlayScreen_Patch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var il = instructions.ToList();
+            il.RemoveRange(2, 3);
+
+            return il;
+        }
+    }
+}
